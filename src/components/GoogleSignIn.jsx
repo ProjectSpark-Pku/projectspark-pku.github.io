a1d4f54208a5c2925a2cb96c1a4f64d7014c8fbe
Horizontal0 <oliver.keith.wijaya@gmail.com>
2026-08-03 20:42:06 +0700

    fix: redirect button

--- src/components/GoogleSignIn.jsx
+++ src/components/GoogleSignIn.jsx
@@ -6,7 +6,7 @@ export default function GoogleSignIn() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: window.location.origin,
+        redirectTo: window.location.origin + import.meta.env.BASE_URL,
       },
     });
 
